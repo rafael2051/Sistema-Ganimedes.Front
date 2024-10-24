f64524f9287aa2ad818037698c186c86f7fca20f
Marcos <mtomasferreira@hotmail.com>
2024-10-24 19:38:46 -0300

    feat: sobe front com erro

--- Front/src/app/formulario/formulario.component.ts
+++ Front/src/app/formulario/formulario.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, forwardRef } from "@angular/core";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatRadioModule } from "@angular/material/radio";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from "@angular/forms";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatIconModule } from "@angular/material/icon";
 @Component({
-  selector: 'app-formulario',
+  selector: "app-formulario",
   standalone: true,
-  imports: [],
-  templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.scss'
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
+  templateUrl: "./formulario.component.html",
+  styleUrl: "./formulario.component.scss",
+  providers: [],
 })
 export class FormularioComponent {
-
+  public forms = new FormGroup({
+    artigosEscrita: new FormControl(0),
+    artigosSubmetidos: new FormControl(0),
+    artigosAceitos: new FormControl(0),
+    atividadesAcademicas: new FormControl(""),
+    atividadesPesquisa: new FormControl(""),
+    declaracaoCCP: new FormControl(""),
+    dificuldades: new FormControl(""),
+  });
+  constructor() {}
 }

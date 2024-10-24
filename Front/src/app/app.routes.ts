93afcad034322a72ad6f50b57159ee37db79fd76
Marcos <mtomasferreira@hotmail.com>
2024-10-23 23:37:32 -0300

    feat: estrutura básica e inicial do frontend do projeto

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { FormularioComponent } from "./formulario/formulario.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "formulario", pathMatch: "full" },
+  { path: "formulario", component: FormularioComponent },
+];

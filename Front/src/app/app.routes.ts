f266251c654b952f9baad5699a234a57f18ad200
Marcos <mtomasferreira@hotmail.com>
2024-11-09 17:22:11 -0300

    feat: implementação da lógica do formulário, navbar, páginas e rotas

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from "@angular/router";
 import { FormularioComponent } from "./formulario/formulario.component";
-import { HomeComponent } from "./home/home.component";
+import { PerfilComponent } from "./perfil/perfil.component";
+import { HistoricoComponent } from "./historico/historico.component";
 
 export const routes: Routes = [
-  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "", redirectTo: "formulario", pathMatch: "full" }, // TODO: Implementar a verificação de autenticação antes de redirecionar
   { path: "formulario", component: FormularioComponent },
+  { path: "perfil", component: PerfilComponent },
+  { path: "historico", component: HistoricoComponent },
 ];

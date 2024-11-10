92a226a2c11c0b96b2dca391b44bb6754d305638
Marcos <mtomasferreira@hotmail.com>
2024-11-10 15:35:32 -0300

    feat: ajuste da lógica do token

--- ganimedes/src/app/utils/auth.guard.ts
+++ ganimedes/src/app/utils/auth.guard.ts
@@ -4,14 +4,15 @@ export const authGuard: CanActivateFn = (route, state) => {
   const token = sessionStorage.getItem('token_expires');
   console.log('token auth guard', token);
 
-  sessionStorage.setItem('token_expires', new Date().toDateString());
+  sessionStorage.setItem('token_teste', new Date().toDateString());
   console.log(
     'acabei de salvar um token simbolico',
-    sessionStorage.getItem('token_expires')
+    sessionStorage.getItem('token_teste')
   );
 
   //TODO: Implementar lógica de validação do token
   if (token) {
+    console.log('token existe e acesso bloqueado');
     return false;
   }
   return true;

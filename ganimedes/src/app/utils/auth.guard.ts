d5b59c480b21dadc1a074476023aa8ab780ad027
Marcos <mtomasferreira@hotmail.com>
2024-11-20 23:19:46 -0300

    feat: ajustes pequenos

--- ganimedes/src/app/utils/auth.guard.ts
+++ ganimedes/src/app/utils/auth.guard.ts
@@ -1,20 +1,21 @@
 import { CanActivateFn } from "@angular/router";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
   const token = sessionStorage.getItem("token");
 
-  // console.log("parametro route", route);
-  // console.log("parametro state", state);
-
-  //TODO: Implementar lógica de validação do token
+  //TODO: Ver se a lógica funciona
   if (token) {
-    const obj = Object.values(token);
+    const obj = JSON.parse(token);
     const now = new Date();
-    // TODO: Validar para corrigir o erro de TS
-    // const exp = new Date(obj["expires"]);
+    const exp = new Date(obj.expires);
 
-    // return exp >= now;
+    if (exp < now) return exp >= now;
   }
 
+  // TODO: mudar para false no futuro e descomentar o router
+  // router.navigate(["login"]);
   return true;
 };
